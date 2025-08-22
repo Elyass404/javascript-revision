3b90e18675e1293afceca670e3930bc8c8cc2978
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-08-22 04:22:06 +0100

    did a simple js exercice with vars and log

--- Revising Basics 01/test.js
+++ Revising Basics 01/test.js
@@ -5,3 +5,34 @@ console.log(
 
 let number = 522;
 console.log(number + 1202);
+
+
+//-----------------------------Exercice N1------------------------------------------------------
+//create a variable name (name) and store your name in it
+//create a variable name (whatDoYouWantToBecomeInLife) and store "programmer" in it
+//create a variable name (gender) and store  your gender in it
+//create a variable name (age) and store  your age in it
+//create a constant name (country) and store  your country in it
+//console log all the variables and the constant you have already created
+
+let name = "ilyass";
+let whatDoYouWantToBecomeInLife = "full stack js developer";
+let gender = "Male";
+let age = 24;
+const country = "morocoo";
+
+//The answer:
+console.log(
+  "hello my name is " +
+    name +
+    ", and i want to become a " +
+    whatDoYouWantToBecomeInLife +
+    " iam a " +
+    gender +
+    " and iam " +
+    age +
+    ", living in " +
+    country +
+    "."
+);
+//-----------------------------End of Exercice N1---------------------------------------------------
