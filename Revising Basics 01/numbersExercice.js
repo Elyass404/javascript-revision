cd78b2e885236fa5f8111015e929f16df6c0c00f
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-08-22 04:42:17 +0100

    did the numbers exercice

---
+++ Revising Basics 01/numbersExercice.js
@@ -0,0 +1,26 @@
+// 1. Create Variable name (firstFavNum) & store your favorite number.
+// 2. Create Variable name (secondFavNum) & store your second fav number.
+// 3. Add (firstFavNum & secondFavNumber).
+// 4. Subtract (firstFavNum & secondFavNumber).
+// 5. Multiply (firstFavNum & secondFavNumber).
+// 6. Divided (firstFavNum & secondFavNumber).
+// 7. Check (firstFavNum Mod secondFavNumber)
+// 8. Check the power of (firstFavNum)
+
+let firstFavNum = 31
+let secondFavNum=7
+let sumFirstAndSecondFavNums = firstFavNum + secondFavNum
+let subFirstAndSecondFavNums = firstFavNum - secondFavNum
+let multiplicationFirstAndSecondFavNums = firstFavNum * secondFavNum
+let divisionFirstAndSecondFavNums = firstFavNum / secondFavNum
+let modFirstAndSecondFavNums = firstFavNum % secondFavNum
+let powerFirstAndSecondFavNums = firstFavNum ** secondFavNum
+
+console.log(firstFavNum)
+console.log(secondFavNum)
+console.log(sumFirstAndSecondFavNums)
+console.log(subFirstAndSecondFavNums)
+console.log(multiplicationFirstAndSecondFavNums)
+console.log(divisionFirstAndSecondFavNums)
+console.log(modFirstAndSecondFavNums)
+console.log(powerFirstAndSecondFavNums)
