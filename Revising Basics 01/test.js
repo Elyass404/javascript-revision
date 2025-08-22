ec6f28e2cc69024883f3d8b3653fa63fed9819d3
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-08-22 03:25:26 +0100

    nothing important

--- Revising Basics 01/test.js
+++ Revising Basics 01/test.js
@@ -1,1 +1,7 @@
-document.getElementById("heading").textContent = "You know the deal";
+// document.getElementById("heading").textContent = "You know the deal";
+console.log(
+  "i mean this is the thing that you shoul do in the main thing of the maing wold "
+);
+
+let number = 522;
+console.log(number + 1202);
