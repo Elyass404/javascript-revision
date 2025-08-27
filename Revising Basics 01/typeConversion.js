735aebce45c72ee6d7f40fdf887f821a21877298
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-08-27 21:32:09 +0100

    added a file to explain type conversion and how to do it

---
+++ Revising Basics 01/typeConversion.js
@@ -0,0 +1,17 @@
+//To change a string into a number 
+let stringNumb = "31"
+//you can use different ways
+let stringToNumber1 = parseInt(stringNumb)
+let stringToNumber2 = Number(stringNumb)
+let stringToNumber3 = +stringNumb
+
+//To change a number into a string
+let number = 33
+let numberToString = number.toString()
+
+//To change a number into a float
+let stringFloat = "31.31"
+let stingToFloat = parseFloat(stringFloat)
+
+
+console.log(typeof(stringToNumber2)) //you can put any variable inside the function to check its type
